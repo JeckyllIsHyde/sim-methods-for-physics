1fb68bd128b3dacf5d6c0a1c25db2c521acfbbfb
Jaime Andres <jacastillol@gmail.com>
2017-09-12 21:04:02 -0500

    Cuerpo class implementation

--- clase20170810/UnBalon.cpp
+++ clase20170810/UnBalon.cpp
@@ -5,6 +5,32 @@ using namespace std;
 
 const double g = 9.81;
 
+class Cuerpo;
+
+class Cuerpo {
+ private:
+  double x,y,Vx,Vy,Fx,Fy,m,R;
+ public:
+  void Inicio(double x0,double y0,double Vx0,double Vy0,double m0,double R0);
+  void CalculeFuerza(void);
+  void Muevase(double dt);
+  double Getx(void) const {return x;};
+  double Gety(void) const {return y;};
+};
+
+void Cuerpo::Inicio(double x0,double y0,double Vx0,double Vy0,double m0,double R0) {
+  x = x0; y = y0; Vx = Vx0; Vy = Vy0; m = m0; R0 = R0;
+}
+
+void Cuerpo::CalculeFuerza(void) {
+  Fx = 0;Fy = -m*g;
+}
+                                  
+void Cuerpo::Muevase(double dt) {
+  x+=Vx*dt;y+=Vy*dt;
+  Vx+=Fx*dt/m;Vy+=Fy*dt/m;
+}
+
 int main(void) {
   double t, dt=0.01;
   Cuerpo balon;
