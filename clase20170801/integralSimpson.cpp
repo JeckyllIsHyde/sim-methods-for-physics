00a97f33bb4f7c1ee86a42352c8b754f1539691d
Jaime Andres <jacastillol@gmail.com>
2017-09-12 20:08:19 -0500

    An example for Simpson's rule

---
+++ clase20170801/integralSimpson.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void) {
+
+  double a, b, alfa=0;
+  cout << ceroPorBiseccion(alfa, 2, 3) << endl;
+  cout << ceroPorBiseccion(alfa, 5, 7) << endl;
+  cout << ceroPorBiseccion(alfa, 7, 9) << endl;
+  cout << ceroPorBiseccion(alfa, 11, 13) << endl;
+    
+  return 0;
+}
