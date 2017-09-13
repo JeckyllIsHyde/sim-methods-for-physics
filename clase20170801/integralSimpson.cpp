a4c15870a11e3daaf1897a161f2c1de60463ba21
Jaime Andres <jacastillol@gmail.com>
2017-09-12 20:13:33 -0500

    Simpson's rule implementation

--- clase20170801/integralSimpson.cpp
+++ clase20170801/integralSimpson.cpp
@@ -3,6 +3,25 @@
 
 using namespace std;
 
+double f(double alfa, double x, double t) {
+  return cos(alfa*t-x*sin(t));
+}
+
+double IntegralPorSimpson(double alfa, double x, double a, double b, double N) {
+  double h = (b-a)/N, suma,t;
+  int i;
+  for ( suma=0, i=0; i<=N; i++ ) {
+    t = a+i*h;
+    if ( i==0 || i==N )
+      suma += f(alfa,x,t);
+    else if ( i%2==0 )
+      suma += 2*f(alfa,x,t);
+    else
+      suma += 4*f(alfa,x,t);    
+  }
+  return suma*h/3;
+}
+
 int main(void) {
 
   double a, b, alfa=0;
