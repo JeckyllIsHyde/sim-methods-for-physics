c52d78bd331ad647307fa1a29844674aa4adc27c
Jaime Andres Castillo Leon <jacastillol@gmail.com>
2017-09-21 12:48:02 -0500

    docs: Add proposal for humanoid simulation

---
+++ propuesta01/prop01.mm
@@ -0,0 +1,95 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1506002316526"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Propuesta 1
+    </p>
+    <p style="text-align: center">
+      Locomocion humanoide, contacto,
+    </p>
+    <p style="text-align: center">
+      impacto y dinamica centroidal
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4"/>
+<node TEXT="Simulacion" POSITION="right" ID="ID_1543090780" CREATED="1506002323768" MODIFIED="1506002335241">
+<edge COLOR="#ff0000"/>
+<node TEXT="Motores fisico" ID="ID_192242353" CREATED="1506002335250" MODIFIED="1506002341447">
+<node TEXT="ODE" ID="ID_1650791484" CREATED="1506002341452" MODIFIED="1506002346309"/>
+<node TEXT="Featherstone&apos;s approach" ID="ID_894574550" CREATED="1506002347130" MODIFIED="1506002363387"/>
+</node>
+<node TEXT="6D-vectors" ID="ID_319523912" CREATED="1506002366521" MODIFIED="1506002373266"/>
+</node>
+<node TEXT="Modelo de contacto" POSITION="right" ID="ID_583940179" CREATED="1506002415886" MODIFIED="1506002423918">
+<edge COLOR="#0000ff"/>
+<node TEXT="Hertz" ID="ID_539195732" CREATED="1506002423924" MODIFIED="1506002427862"/>
+<node TEXT="Mozah" ID="ID_1257937700" CREATED="1506002428536" MODIFIED="1506002444649"/>
+</node>
+<node TEXT="Dinamica centroidal" POSITION="right" ID="ID_1772502658" CREATED="1506002447392" MODIFIED="1506002460102">
+<edge COLOR="#00ff00"/>
+<node TEXT="Momento lineal" ID="ID_565957192" CREATED="1506002471041" MODIFIED="1506002479256"/>
+<node TEXT="Momento angular" ID="ID_1411318346" CREATED="1506002460105" MODIFIED="1506002469971"/>
+</node>
+<node TEXT="Generaci&#xf3;n de trayectorias" POSITION="right" ID="ID_1011506672" CREATED="1506002489529" MODIFIED="1506002495915">
+<edge COLOR="#ff00ff"/>
+</node>
+</node>
+</map>
