20ed7a4a67ac984ad64f34f36f58a1347e17d6c5
Jaime Andres <jacastillol@gmail.com>
2017-09-28 10:44:01 -0500

    feat: Add a test for MonteCarlo

---
+++ clase20170928/MiPrimerMonteCarlo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include "Random64.h"
+
+using namespace std;
+
+int main() {
+  Crandom ran64(1);
+  int i;
+  double mu=3,sigma=2;
+
+  for (i=0;i<1e4;i++)
+    cout << sigma*sqrt(-2*log(ran64.r()))*cos(2*M_PI*ran64.r())+mu << endl;
+
+  return 0;
+}
